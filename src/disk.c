b2e9c82cb8f25be1e8e2435d29ea0842aa8fc001
joxguedez <120581740+joxguedex@users.noreply.github.com>
2026-01-09 21:21:13 -0400

    disk .c y disk.h

--- src/disk.c
+++ src/disk.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include <string.h>
 #include "disk.h"
+
+// Variable privada del archivo (Hardware encapsulado)
+static Disk fisic_disk;
+
+// Inicializa el disco llenándolo de ceros
+void init_disk(void) {
+    memset(&fisic_disk, 0, sizeof(Disk));
+    printf("[HARDWARE] Disco inicializado correctamente.\n");
+}
+
+// Lectura: Hardware -> Ram (destiny)
+int read_sector(int t, int c, int s, char *destiny) {
+    //validacion para no salirse de los limites
+    if (t < 0 || t >= NUM_TRACKS ||
+        c < 0 || c >= NUM_CYLINDERS ||
+        s < 0 || s >= NUM_SECTORS) {
+        return -1;
+    }
+
+    memcpy(destiny, fisic_disk.platter[t][c][s].data, TAM_SECTOR);
+
+    return 0;
+}
+
+// Escritura: Ram (origin) -> Hardware
+int write_sector(int t, int c, int s, char *origin) {
+
+    if (t < 0 || t >= NUM_TRACKS ||
+        c < 0 || c >= NUM_CYLINDERS ||
+        s < 0 || s >= NUM_SECTORS) {
+        return -1;
+    }
+
+
+    memcpy(fisic_disk.platter[t][c][s].data, origin, TAM_SECTOR);
+
+    return 0;
+}
--- src/disk.h
+++ src/disk.h
@@ -1,5 +1,28 @@
 #ifndef DISK_H
 #define DISK_H
 
+#include <pthread.h>
+#define NUM_TRACKS 10
+#define NUM_CYLINDERS 10
+#define NUM_SECTORS 100
+#define TAM_SECTOR 9
+
+//structs
+
+typedef struct {
+    unsigned char data[TAM_SECTOR];
+} Sector;
+
+typedef struct {
+    Sector platter[NUM_TRACKS][NUM_CYLINDERS][NUM_SECTORS];
+} Disk;
+
+//funciones
+void init_disk(void);
+
+//t = track, c = cylinder, s = sector
+int read_sector(int t, int c, int s, char *destiny);
+
+int write_sector(int t, int c, int s, char *origin);
 
 #endif
